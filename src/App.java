2aa9cb68a3cf0255faa930fd975cccd2a1e8e1d8
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2023-07-28 16:48:03 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int num = 10;
+        System.out.println("Valor de num int: " + num);
+        Integer num2 = (Integer) num;
+        System.out.println("Wrapper class de int, valor: " + num2);
+    }
+}
